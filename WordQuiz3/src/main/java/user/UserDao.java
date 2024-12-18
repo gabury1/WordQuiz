0bc0f2d822d7472ea828163f1ee8daaf67882fb9
kcjsend5 <kcjsend5@naver.com>
2024-12-18 20:46:46 +0900

    마이페이지,유저 정보페이지 기능 완성

--- WordQuiz3/src/main/java/user/UserDao.java
+++ WordQuiz3/src/main/java/user/UserDao.java
@@ -5,18 +5,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 public class UserDao {
 
-	final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	final static String JDBC_URL = "jdbc:mysql://localhost:3306/wordquiz";
+	final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	final String JDBC_URL = "jdbc:mysql://localhost:3306/wordquiz";
 
 	// DB 연결을 가져오는 메서드, DBCP를 사용하는 것이 좋음
 	public Connection open() {
@@ -28,6 +21,78 @@ public class UserDao {
 			e.printStackTrace();
 		}
 		return conn;
+	}
+	public UserDto getUser(int user_no)throws SQLException { //유저 정보 출력
+		Connection conn = open();
+		UserDto users = new UserDto();
+		String sql = "select user_no, nickname, img ,age, STR_TO_DATE(created_at,'%Y-%m-%d %H:%i:%s') as cdate ,content, id, password from Users where user_no = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1, user_no);
+		ResultSet rs = pstmt.executeQuery();
+		rs.next();
+		try(conn; pstmt; rs){
+			users.setUser_no(rs.getInt("user_no"));
+			users.setImg(rs.getString("img"));
+			users.setNickname(rs.getString("nickname"));
+			users.setAge(rs.getInt("age"));
+			users.setCreate_at(rs.getString("cdate"));
+			users.setContent(rs.getString("content"));
+			users.setId(rs.getString("id"));
+			users.setPassword(rs.getString("password"));
+			return users;
+		}
+	}
+	
+	//우진 만든거
+	public void insertUser(UserDto user) throws Exception {
+	    String sql = "INSERT INTO member (id, password, nickname, age) VALUES (?, ?, ?, ?)";
+	    System.out.println("[DEBUG] SQL 준비: " + sql);
+	    
+	    try (Connection conn = open(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+	        System.out.println("[DEBUG] DB 연결 성공");	        
+	        System.out.println("[DEBUG] 입력값 확인: " + user.getId() + ", " + user.getPassword() + ", " + user.getNickname() + ", " + user.getAge());
+	        pstmt.setString(1, user.getId());
+	        pstmt.setString(2, user.getPassword());
+	        pstmt.setString(3, user.getNickname());
+	        pstmt.setInt(4, user.getAge());
+	        
+	        int result = pstmt.executeUpdate();
+	        System.out.println("[DEBUG] SQL 실행 완료, 반영된 행 수: " + result);
+	    } catch (Exception e) {
+	        System.out.println("[ERROR] SQL 실행 중 오류 발생");
+	        e.printStackTrace();
+	        throw e; // 상위 메서드에 예외 전달
+	    }
+	}
+//우진 만든거
+	
+	
+	
+	public void delUser(int user_no)throws SQLException { // 유저 삭제
+		Connection conn = open();
+
+		String sql = "delete from Users where user_no = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(pstmt;conn){
+			pstmt.setInt(1, user_no);
+			if(pstmt.executeUpdate() == 0) {
+				throw new SQLException("DB에러");
+			}
+		}
+	}
+	public void updateUsers(UserDto u) throws Exception { //유저 수정
+		Connection conn = open();
+		String sql = "update Users set password = ?, nickname = ?, age = ?, content=?,img=? where user_no = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(conn; pstmt) {
+			pstmt.setString(1, u.getPassword());
+			pstmt.setString(2, u.getNickname());
+			pstmt.setInt(3, u.getAge());
+			pstmt.setString(4,u.getContent());
+			pstmt.setString(5,u.getImg());
+			pstmt.setInt(6, u.getUser_no());
+			pstmt.executeUpdate();
+		}
 	}
 
 }
--- WordQuiz3/src/main/webapp/user/myPageView.jsp
+++ WordQuiz3/src/main/webapp/user/myPageView.jsp
@@ -1,18 +1,65 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>마이페이지</title>
+<title>유저 설정</title>
+<script>
+	function btnClick() {
+	  const mydiv = document.getElementById('my-div');
+	  
+	  if(mydiv.style.display === 'none') {
+	    mydiv.style.display = 'block';
+	  }else {
+	    mydiv.style.display = 'none';
+	  }
+	}
+</script>
 </head>
 <body>
-
-	<h1> 마이페이지 입니다. </h1>
-	<h2> 로그인 상태가 아니라면 로그인 페이지로 리다이렉트 됩니다.</h2>
-	<h2> 세션 정보를 확인하여 해당 유저가 아니라면 진입이 거부되어야합니다. (메인페이지로 리다이렉트) </h2>
-	<a href="/WordQuiz3/quiz.nhn?action=mainPage"> 메인페이지 </a>
-	<button>수정하기</button>
-	
+	<div>
+		<img src="${users.img}">
+		<div>
+			<p>자기소개: ${users.content}</p>
+			<p>닉네임:${users.nickname}</p>
+			<p>ID:${users.id}</p>
+			<p>PW:${users.password}</p>
+			<p>나이:${users.age}</p>
+		</div>
+		<hr>
+		<div>
+			<button type="button" onclick='btnClick()'>수정</button>
+			<div id='my-div' style="display:none;">
+				<form method="post" action="/WordQuiz3/quiz.nhn?action=updateUser" enctype="multipart/form-data">
+					<label class="form-label">이미지</label><br>
+					<input type="file" name="file"><br>
+					<label class="form-label">닉네임</label><br>
+					<input type="text"name="nickname"><br> 
+					<label class="form-label">비밀번호</label><br>
+					<input type="text"name="password"><br>
+					<label class="form-label">나이</label><br> 
+					<input type="number"name="age"><br>
+					<label class="form-label">자기소개</label><br>
+					<textarea cols="50" rows="5" name="content"></textarea><br>
+					<button type="submit">저장</button>
+					<hr>
+				</form>
+			</div>
+			<button type="button" onclick="location.href='/WordQuiz3/quiz.nhn?action=deleteUser'">삭제</button>
+		</div>
+		<h2>댓글 목록</h2>
+		<hr>
+		<ul class="list-group">
+			<c:forEach var="reply" items="${replylist}" varStatus="status">
+				<li>
+					<img src="${reply.img}">
+					<a>[${status.count}] ${reply.nickname}, ${reply.content}, ${reply.create_at}</a>
+				</li>
+			</c:forEach>
+		</ul>
+		<hr>
+	</div>
 </body>
 </html>
