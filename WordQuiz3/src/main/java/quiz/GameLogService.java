335dd9b63a26cf1194388f09288064c72155bd6a
UJIN\dnwls <dnwls0254@naver.com>
2024-12-22 19:22:01 +0900

    ujin jin

--- WordQuiz3/src/main/java/quiz/GameLogService.java
+++ WordQuiz3/src/main/java/quiz/GameLogService.java
@@ -5,4 +5,15 @@ public class GameLogService
 	
 	
 	
+<<<<<<< HEAD
+=======
+	// 게임 로그를 불러옵니다. (랭킹 불러오기)
+	public List<RankingDto> getRanking()
+	{
+		var list = gameLogDao.getRanking();
+		
+		return list;
+	}
+	
+>>>>>>> parent of 54b4104 (Ujin)
 }
